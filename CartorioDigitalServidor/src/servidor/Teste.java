432186b91648d3d0d97c0d9723a750237f45f49a
SaitamaXD <30640347+SaitamaXD@users.noreply.github.com>
2019-07-31 19:31:22 -0300

    Testando codificação de strings

--- CartorioDigitalServidor/src/servidor/Teste.java
+++ CartorioDigitalServidor/src/servidor/Teste.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package servidor;
+
 import java.util.Arrays;
 import org.apache.commons.codec.binary.Base64;
 
@@ -12,29 +13,30 @@ import org.apache.commons.codec.binary.Base64;
  * @author User
  */
 public class Teste {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         String helloWorld = "Hello World!";
- 
-          helloWorld = Base64.encodeBase64String(helloWorld.getBytes());
-           
-       System.out.println("String codificada " + helloWorld);
-  
-       //
-       // Decodifica uma string anteriormente codificada usando o método decodeBase64 e
-       // passando o byte[] da string codificada
-       //
-       byte[] decoded = Base64.decodeBase64(helloWorld.getBytes());
-  
-       //
-       // Imprime o array decodificado
-       //
-       System.out.println(Arrays.toString(decoded));
-  
-       //
-       // Converte o byte[] decodificado de volta para a string original e imprime
-       // o resultado.
-       //
-       String decodedString = new String(decoded);
-       System.out.println(helloWorld + " = " + decodedString);
+
+        helloWorld = Base64.encodeBase64String(helloWorld.getBytes());
+
+        System.out.println("String codificada " + helloWorld);
+
+        //
+        // Decodifica uma string anteriormente codificada usando o método decodeBase64 e
+        // passando o byte[] da string codificada
+        //
+        byte[] decoded = Base64.decodeBase64(helloWorld.getBytes());
+
+        //
+        // Imprime o array decodificado
+        //
+        System.out.println(Arrays.toString(decoded));
+
+        //
+        // Converte o byte[] decodificado de volta para a string original e imprime
+        // o resultado.
+        //
+        String decodedString = new String(decoded);
+        System.out.println(helloWorld + " = " + decodedString);
     }
 }
